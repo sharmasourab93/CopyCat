44b1772af13678612aa102f250cc47eff8e1baf2
Sourab <sharmasourab93@gmail.com>
2020-08-25 21:29:38 +0530

    CopyCat 25th August Commits

--- CopyCat/CopyCat/urls.py
+++ CopyCat/CopyCat/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^', include('mainapp.urls')),
+    path('', include('mainapp.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
